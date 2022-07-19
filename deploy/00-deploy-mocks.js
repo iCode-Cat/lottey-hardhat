9391e06ea2798b5862fd6fc52c8aa695fa4a4517
iCode-Cat <devranboyac8@gmail.com>
2022-07-19 23:58:38 +0300

    complete deploy with errors

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,4 +1,8 @@
+const { ethers } = require("ethers")
 const { developmentChains } = require("../helper-hardhat-config")
+
+const BASE_FEE = ethers.utils.parseEther("0.25")
+const GAS_PRICE_LINK = 1e9
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
@@ -11,6 +15,9 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         await deploy("VRFCoordinatorV2Mock", {
             from: deployer,
             log: true,
+            args: [BASE_FEE, GAS_PRICE_LINK],
         })
+        log("Mocks Deployed!")
+        log("----------------------------------------------")
     }
 }
--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,13 +1,51 @@
-const { network } = require("../hardhat.config")
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let VRFCoordinatorV2Address, subscriptionId
 
+    if (developmentChains.includes(network.name)) {
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        VRFCoordinatorV2Address = VRFCoordinatorV2Mock
+        const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait(1)
+        subscriptionId = transactionReceipt.events[0].args.subId
+        await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+    } else {
+        VRFCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
+    }
+
+    const enteranceFee = networkConfig[chainId]["enteranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const callbackGaslimit = networkConfig[chainId]["callbackGasLimit"]
+    const interval = networkConfig[chainId]["interval"]
+
+    const args = [
+        VRFCoordinatorV2Address,
+        enteranceFee,
+        gasLane,
+        subscriptionId,
+        callbackGaslimit,
+        interval,
+    ]
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: [],
+        args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(raffle.address, args)
+    }
+    log("-------------------")
 }
+
+module.exports.tags = ["all", "raffle"]
